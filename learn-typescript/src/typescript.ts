e296dee7a40e3c0928301cff3794f61c1a42c2e6
Hung Ta <hungta357@gmail.com>
2026-09-16 19:31:07 +0700

    adsadsadsa\

--- learn-typescript/src/typescript.ts
+++ learn-typescript/src/typescript.ts
@@ -1,1 +1,18 @@
 console.log("typescript");
+
+
+let myName: string = "hoadv";
+myName = "28";
+
+let dihockhong: boolean = true;
+dihockhong = false;
+
+enum ProductRate {
+    low = "thap",
+    hight = "cao",
+}
+const Product: { name:string; rate: ProductRate } ={
+    name: "laptop",
+    rate: ProductRate.low,
+};
+console.log(ProductRate);
